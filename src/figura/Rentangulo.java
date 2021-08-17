3a6acde24037930fd2e8fe3d5d514e5e67f41419
celib <celib@DESKTOP-QI950CO.lan>
2021-08-17 15:08:22 -0300

    se agrega toString a Rectangulo

--- src/figura/Rentangulo.java
+++ src/figura/Rentangulo.java
@@ -1,6 +1,18 @@
 package figura;
 
 public class Rentangulo {
+	double ancho, alto;
+
+	public Rentangulo(double ancho, double alto) {
+		this.ancho = ancho;
+		this.alto = alto;
+	}
+
+	@Override
+	public String toString() {
+		return "Rentangulo [ancho=" + ancho + ", alto=" + alto + "]";
+	}
+	
 	
 	
 
