3cf26772803582321e02da9b6eca8c67add6c793
celib <celib@DESKTOP-QI950CO.lan>
2021-08-17 15:05:20 -0300

    Se agrega calculo del perimetro

--- src/figura/Rentangulo.java
+++ src/figura/Rentangulo.java
@@ -1,5 +1,17 @@
 package figura;
 
 public class Rentangulo {
+	
+	double ancho, alto;
+
+	public Rentangulo(double ancho, double alto) {
+		this.ancho = ancho;
+		this.alto = alto;
+	}
+	
+	public double getPerimetro() {
+		return this.alto*2+this.ancho*2;
+	}
+	
 
 }
